5e98ac9b09cf849647a15cfacdb99ec8abbc55aa
annedorene <dorene.chua@obf.ateneo.edu>
2022-05-18 23:28:27 +0800

    Intro to Flask Pt. 2
    Intro to Flask Pt. 2

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask import render_template
 from flask import request
+from flask import session
 import database as db
+import authentication
+import logging
 
 app = Flask(__name__)
+
+# Set the secret key to some random bytes.
+# Keep this really secret!
+app.secret_key = b's@g@d@c0ff33!'
+
+logging.basicConfig(level=logging.DEBUG)
+app.logger.setLevel(logging.INFO)
 
 @app.route('/')
 def index():
@@ -36,3 +46,80 @@ def branchdetails():
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html', page="About Us")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/auth', methods = ['GET', 'POST'])
+def auth():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    is_successful, user = authentication.login(username, password)
+    app.logger.info('%s', is_successful)
+    if(is_successful):
+        session["user"] = user
+        return redirect('/')
+    else:
+        error = "Invalid username or password. Please try again."
+        return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop("user",None)
+    session.pop("cart",None)
+    return redirect('/')
+
+@app.route('/addtocart')
+def addtocart():
+    code = request.args.get('code', '')
+    product = db.get_product(int(code))
+    item=dict()
+    # A click to add a product translates to a
+    # quantity of 1 for now
+
+    item["qty"] = 1
+    item["name"] = product["name"]
+    item["subtotal"] = product["price"]*item["qty"]
+    item["code"] = code
+
+    if(session.get("cart") is None):
+        session["cart"]={}
+
+    cart = session["cart"]
+    cart[code]=item
+    session["cart"]=cart
+    return redirect('/cart')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/updatecartitem', methods = ['GET', 'POST'])
+def updatecartitem():
+    qty = request.form.get("qty")
+    code = request.form.get("code")
+    update = request.form.get("Update")
+    delete = request.form.get("Delete")
+    product = db.get_product(int(code))
+    item = dict()
+
+    if update == 'Update':
+        item["qty"] = int(qty)
+        item["name"] = product["name"]
+        item["subtotal"] = product["price"]*item["qty"]
+        item["code"] = code
+
+        cart = session["cart"]
+        cart[code] = item
+        session["cart"] = cart
+
+        return redirect('/cart')
+
+    elif delete == 'Delete':
+        cart = session["cart"]
+        cart.pop(code, None)
+        session["cart"] = cart
+
+        return redirect('/cart')
